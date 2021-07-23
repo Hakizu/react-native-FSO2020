15a3d13a9ef21aa8e8295567d07e891667691c40
Jan Krumrein <hakisuj@gmail.com>
2021-07-23 20:50:20 +0200

    :sparkles: Exercise 10.10

--- src/theme.js
+++ src/theme.js
@@ -1,4 +1,5 @@
 import Constants from "expo-constants";
+import { Platform } from "react-native";
 
 const theme = {
     colors: {
@@ -11,7 +12,11 @@ const theme = {
         subheading: 16,
     },
       fonts: {
-        main: 'System',
+        main: Platform.select({
+          android: 'Roboto',
+          ios: 'Arial',
+          default: 'System',
+        })
     },
       fontWeights: {
         normal: '400',
